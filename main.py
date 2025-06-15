7861d37ad81af77a7eda9349fbde5111d4f1db1c
Daniel-Parada-da-Silva <daniel.parada.da.silva@gmail.com>
2025-06-15 22:37:05 +0200

    Setting the project - first assignments

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 import os
+import sys
+
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
+verbose = "--verbose" in sys.argv
+
+if len(sys.argv) < 2:
+    print("Missing prompt")
+    sys.exit(1)
+user_prompt = sys.argv[1]
+
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
+
+
+response = client.models.generate_content(
+    model='gemini-2.0-flash-001', contents=messages
+)
+
+if verbose:
+    print(f"User prompt: {user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}\n")
+
+print(response.text)
