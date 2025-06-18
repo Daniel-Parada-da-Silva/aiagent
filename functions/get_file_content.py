6bc8ae59dbbc07693b2d84016f32ddeab3c37306
Daniel-Parada-da-Silva <daniel.parada.da.silva@gmail.com>
2025-06-18 08:33:50 +0200

    get_file_content

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -9,7 +9,17 @@ def get_file_content(working_directory, file_path):
     if os.path.isdir(abs_target):
         return f'Error: File not found or is not a regular file: "{file_path}"'
     
-    MAX_CHARS = 10000
+    try:
+        MAX_CHARS = 10000
+
+        with open(abs_target, "r") as f:
+            file_content_string = f.read(MAX_CHARS + 1)
+            f.close()
+        
+        if len(file_content_string) > MAX_CHARS:
+            file_content_string = file_content_string[:MAX_CHARS] + f'[...File "{file_path}" truncated at 10000 characters]'
+
+        return file_content_string
+    except Exception as error:
+        return f"Error: {error}"
     
-    with open(file_path, "r") as f:
-        file_content_string = f.read(MAX_CHARS)
--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import unittest
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
 
 class TestCalculator(unittest.TestCase):
@@ -17,6 +18,22 @@ class TestCalculator(unittest.TestCase):
 
     def test_calculator_parent(self):
         result = get_files_info("calculator", "../")
+        print(result)
+    
+    # def test_lorem_ipsum(self):
+    #     result = get_file_content("calculator", "lorem.txt")
+    #     print(result)
+
+    def test_file_content_calculator_main(self):
+        result = get_file_content("calculator", "main.py")
+        print(result)
+
+    def test_file_content_calculator_pkg_calculator(self):
+        result = get_file_content("calculator", "pkg/calculator.py")
+        print(result)
+
+    def test_file_content_calculator_bin(self):
+        result = get_file_content("calculator", "/bin/cat")
         print(result)
 
 
