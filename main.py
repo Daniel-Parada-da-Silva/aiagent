85d95ece627025e7e8aa188db67d49692975aea1
Daniel-Parada-da-Silva <daniel.parada.da.silva@gmail.com>
2025-06-19 10:30:06 +0200

    refactoring

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+from call_function import available_functions
+
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
@@ -25,26 +27,6 @@ When a user asks a question or makes a request, make a function call plan. You c
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
 """
 
-schema_get_files_info = types.FunctionDeclaration(
-    name="get_files_info",
-    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
-    parameters=types.Schema(
-        type=types.Type.OBJECT,
-        properties={
-            "directory": types.Schema(
-                type=types.Type.STRING,
-                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
-            ),
-        },
-    ),
-)
-
-available_functions = types.Tool(
-    function_declarations=[
-        schema_get_files_info,
-    ]
-)
-
 config=types.GenerateContentConfig(
     tools=[available_functions], system_instruction=system_prompt
 )
@@ -63,4 +45,8 @@ if verbose:
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}\n")
 
-print(f"Calling function: {response.function_calls}")
+if not response.function_calls:
+    print(response.text)
+
+for function_call_part in response.function_calls:
+    print(f"Calling function: {function_call_part.name}({function_call_part.args})")
