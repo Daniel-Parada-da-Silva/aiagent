f8c913a63e9e06110146369b27b070c902fa7055
Daniel-Parada-da-Silva <daniel.parada.da.silva@gmail.com>
2025-06-20 15:17:53 +0200

    call_funtion alpha

--- call_function.py
+++ call_function.py
@@ -13,3 +13,11 @@ available_functions = types.Tool(
         schema_run_python_file
     ]
 )
+
+def call_function(function_call_part, verbose=False):
+    if verbose:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        print(f" - Calling function: {function_call_part.name}")
+    
+    
